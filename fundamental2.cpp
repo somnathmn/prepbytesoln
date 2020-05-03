c746cbbeab51cfcb6403a2c9404607f8bc68549b
somnathmn <39146912+somnathmn@users.noreply.github.com>
2020-05-03 15:08:12 +0530

    Create fundamental2.cpp

---
+++ fundamental2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  char i;
+  cin>>i;
+   
+    if (i =='p' or i =='P')
+     { 
+         cout << "PrepBytes"; 
+     }
+     else if (i =='z' or i =='Z' ) 
+    { 
+        cout << "Zenith";
+      
+    }
+    else if (i =='E' or i =='e' ) 
+    { 
+        cout << "Expert Coder";
+      
+    }
+    else if (i =='d' or i =='D' ) 
+    { 
+        cout << "Data Structure";
+      
+    }
+  
+  return 0;
+}
